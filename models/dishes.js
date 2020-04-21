d2e4d81be6fe59abfd3cdf3f7f2c54b72d42632d
Ossama Ismaili <oussamaismaili06@gmail.com>
2020-04-21 23:34:48 +0000

    Mongoose Population

--- models/dishes.js
+++ models/dishes.js
@@ -3,7 +3,7 @@ const Schema=mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
-const commentSchema=new Schema({
+var commentSchema = new Schema({
     rating:  {
         type: Number,
         min: 1,
@@ -14,14 +14,13 @@ const commentSchema=new Schema({
         type: String,
         required: true
     },
-    author:  {
-        type: String,
-        required: true
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 }, {
     timestamps: true
 });
-
 const dishSchema = new Schema({
     name: {
         type: String,
